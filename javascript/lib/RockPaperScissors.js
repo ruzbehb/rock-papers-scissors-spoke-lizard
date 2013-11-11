ef652da67d997d9720362bcdd997b6530ff54647
Ruzbeh Bacha <ruzbehbacha@gmail.com>
2013-11-11 13:49:20 +0000

    working version

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -32,10 +32,14 @@ Game.prototype.winner = function() {
 
 	if (this.player1.pick == this.player2.pick) {
 		return null;
-}
-	if ((this.pairs[this.player1.pick][0] == this.player2.pick) || (this.pairs[this.player1.pick][1] == this.player2.pick)) {
-		return this.player1;
-}
+	}
+
+	if (this.pairs[this.player1.pick].indexOf(this.player2.pick) != -1) {
+		return this.player1
+	}
+	// if ((this.pairs[this.player1.pick][0] == this.player2.pick) || (this.pairs[this.player1.pick][1] == this.player2.pick)) {
+	// 	return this.player1;
+	// }
 	return this.player2;
 
 }
